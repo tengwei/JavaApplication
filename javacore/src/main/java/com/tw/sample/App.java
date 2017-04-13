0835aa8a1a576cec04efd1a37f5cb3f35f20c778
tengwei <tengweichen@126.com>
2017-04-13 15:35:56 +0800

    123

---
+++ javacore/src/main/java/com/tw/sample/App.java
@@ -0,0 +1,13 @@
+package com.tw.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World333333333333!" );
+    }
+}
